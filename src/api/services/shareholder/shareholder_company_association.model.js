e2d9c5922f57460b838e7b3fba049ae6b0e049e7
Ravindra Kumar <ravindra.kumar@onefitstop.com>
2022-12-07 00:32:27 +0530

    Add quantity

--- src/api/services/shareholder/shareholder_company_association.model.js
+++ src/api/services/shareholder/shareholder_company_association.model.js
@@ -85,7 +85,7 @@ const shareholderCompanyAssociationSchema = new mongoose.Schema(
         quantity: {
             type: Number,
             default: null
-        },        
+        },
         createdAt: {
             type: Date,
             default: Date.now
